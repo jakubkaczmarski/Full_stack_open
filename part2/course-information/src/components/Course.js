b6266309b59c2fc939ea2d6d458bfa53bcce7479
jakubkaczmarski <kuba.kaczmarski312@gmail.com>
2023-01-17 17:30:51 +0100

    Course information part 2

---
+++ part2/course-information/src/components/Course.js
@@ -0,0 +1,43 @@
+const Header = ({ course }) => <h1>{course.name}</h1>
+
+const Total = ({ sum }) => <h3> <p>Total of {sum}</p> </h3>
+
+const Part = ({ part }) => 
+{
+  console.log("Here ", part)
+  return (
+  <p>
+    {part.name} {part.exercises}
+  </p>
+  )
+}
+
+const Content = ({ parts }) => 
+{
+  let itemList=[];
+  parts.forEach((item,index)=>{
+    itemList.push( <Part part={item}/>)
+  })
+  return (
+    <>
+    {itemList}
+    </>
+  )
+}
+
+const Course = ({course}) =>
+{
+    return (
+      <>
+      <Header course={course} /> 
+      <Content parts={course.parts} />
+      <Total sum={course.parts.reduce((sum, part) => {
+        return sum + part.exercises
+      }, 0)} />
+      </>
+    )
+}
+
+
+
+export default Course;
